cdd1e2c9c6f2edda255a5fbe0d25cd3537fcc598
Ruchitha <73153896+ruchi-lab@users.noreply.github.com>
2025-02-25 00:53:42 -0500

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://irdhikhtcsfqbjekbyxv.supabase.co";
-const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyZGhpa2h0Y3NmcWJqZWtieXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzMDc2MTcsImV4cCI6MjA1NDg4MzYxN30.qcDbcBWPwpCfpF2V_xG5jwrYjZT_ErWk7TVDEYZC28w";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY); 
